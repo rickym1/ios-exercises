acf2d6758ff0835d7da0ea2118a56cae81d78660
Rick Matta <rickmatta@gmail.com>
2015-06-10 12:12:28 -0400

    assignment 7 complete

--- BlocExercises/BlocExercises/SimpleCalculator.m
+++ BlocExercises/BlocExercises/SimpleCalculator.m
@@ -12,17 +12,22 @@
 
 - (NSInteger) increaseNumberBy1:(NSInteger) number {
     /* WORK HERE */
-    return number;
+    NSInteger returnNumber = number+1;
+    return returnNumber;
 }
 
 - (NSInteger) addNumber:(NSInteger) number1 toNumber:(NSInteger) number2 {
     /* WORK HERE */
-    return number1;
+    
+    
+    return number1+number2;
 }
 
 - (NSInteger) remainderOfNumber:(NSInteger) dividend dividedByNumber:(NSInteger) divisor {
     /* WORK HERE */
-    return 0;
+    
+    NSInteger remainder = dividend % divisor;
+    return remainder;
 }
 
 @end
