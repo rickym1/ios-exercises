db31c4e2aa735bba3ad7be1d3de21520964b1b9b
Rick Matta <rickmatta@gmail.com>
2015-06-20 20:28:00 -0400

    3

--- SwiftExercises.playground/section-1.swift
+++ SwiftExercises.playground/section-1.swift
@@ -105,3 +105,6 @@ let cerealArray = ["Golden Grahams", "Cheerios", "Trix", "Cap'n Crunch OOPS! All
 
 // Use a closure to sort this array alphabetically
 // WORK HERE
+let sortAA = { (a:String, b:String) -> Bool in return a < b }
+let sortedCereal = sorted(cerealArray, sortAA)
+
